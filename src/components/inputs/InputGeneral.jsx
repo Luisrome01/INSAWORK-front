9c238b7afd2da8ba57d9032c36c632a02eaa683a
Luis Romero <luisonrome01@gmail.com>
2024-08-27 18:02:22 -0400

    actualizar componentes

--- src/components/inputs/InputGeneral.jsx
+++ src/components/inputs/InputGeneral.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "./css/InputGeneral.css";
 
 const InputGeneral = ({
@@ -12,8 +13,11 @@ const InputGeneral = ({
   borderRadius,
   shadow,
   name,
-  onEnter
+  onEnter,
+  isPassword,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const style = {
     width: width,
     height: height,
@@ -22,24 +26,35 @@ const InputGeneral = ({
     boxShadow: shadow,
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <>
       <div className="containerName">
         <span>{name}</span>
       </div>
-      <input
-        type={type}
-        placeholder={placeholder}
-        style={style}
-        value={value}
-        onChange={onChange}
-        className="InputGeneral"
-        onKeyDown={(event) => {
-          if (event.key === "Enter" && typeof onEnter === "function") {
-            onEnter();
-          }
-        }}
-      />
+      <div className="input-container">
+        <input
+          type={isPassword && !showPassword ? "password" : type}
+          placeholder={placeholder}
+          style={style}
+          value={value}
+          onChange={onChange}
+          className="InputGeneral"
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && typeof onEnter === "function") {
+              onEnter();
+            }
+          }}
+        />
+        {isPassword && (
+          <div className="icon-container" onClick={togglePasswordVisibility}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- src/components/modal/ModalResetPassword.jsx
+++ src/components/modal/ModalResetPassword.jsx
@@ -1,79 +1,102 @@
 import React, { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 import "./css/ModalResetPassword.css";
-import MessageBar, { showErrorMessage, showSuccessMessage } from "../messageBar/MessageBar";
+import MessageBar, {
+  showErrorMessage,
+  showSuccessMessage,
+} from "../messageBar/MessageBar";
+import InputGeneral from "../inputs/InputGeneral";
 
 const ModalResetPassword = () => {
-    const [email, setEmail] = useState("");
-    const [messageBar, setMessageBar] = useState(null);
+  const [email, setEmail] = useState("");
+  const [messageBar, setMessageBar] = useState(null);
 
-    const displayMessage = (messageComponent) => {
-        setMessageBar(messageComponent);
+  const displayMessage = (messageComponent) => {
+    setMessageBar(messageComponent);
+    setTimeout(() => {
+      setMessageBar(null);
+    }, 3000);
+  };
+
+  const handlePasswordReset = async () => {
+    if (!email) {
+      displayMessage(
+        showErrorMessage("Por favor, ingrese su correo electrónico.", "center")
+      );
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/forgot-password", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        displayMessage(
+          showSuccessMessage("Correo enviado exitosamente.", "center")
+        );
         setTimeout(() => {
-            setMessageBar(null);
+          handleCloseModal();
         }, 3000);
-    };
+      } else {
+        displayMessage(
+          showErrorMessage(result.msg || "Error al enviar el correo.", "center")
+        );
+      }
+    } catch (error) {
+      displayMessage(
+        showErrorMessage(
+          "Error de conexión. Inténtelo de nuevo más tarde.",
+          "center"
+        )
+      );
+    }
+  };
 
-    const handlePasswordReset = async () => {
-        if (!email) {
-            displayMessage(showErrorMessage("Por favor, ingrese su correo electrónico.", "center"));
-            return;
-        }
+  const handleCloseModal = () => {
+    window.location.reload();
+  };
 
-        try {
-            const response = await fetch("http://localhost:3000/forgot-password", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email }),
-            });
-
-            const result = await response.json();
-
-            if (response.ok) {
-                displayMessage(showSuccessMessage("Correo enviado exitosamente.", "center"));
-                setTimeout(() => {
-                    handleCloseModal();
-                }, 3000);
-            } else {
-                displayMessage(showErrorMessage(result.msg || "Error al enviar el correo.", "center"));
-            }
-        } catch (error) {
-            displayMessage(showErrorMessage("Error de conexión. Inténtelo de nuevo más tarde.", "center"));
-        }
-    };
-
-    const handleCloseModal = () => {
-        // Recargar la página al cerrar el modal
-        window.location.reload();
-    };
-
-    return (
-        <div className="resetPasswordModalContainer">
-            <div className="resetPasswordModalBackgroundBlur"></div>
-            <div className="resetPasswordModalContent">
-                <div className="resetPasswordModalHeader">
-                    <h2 className="resetPasswordModalTitle">Recuperar Contraseña</h2>
-                    <button className="resetPasswordCloseButton" onClick={handleCloseModal}>
-                        X
-                    </button>
-                </div>
-                <div className="resetPasswordModalBody">
-                    {messageBar}
-                    <input 
-                        type="email"
-                        placeholder="Correo Electrónico"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="resetPasswordInputField"
-                    />
-                    <button className="resetPasswordSubmitButton" onClick={handlePasswordReset}>
-                        Enviar Correo
-                    </button> 
-                </div>
-            </div> 
+  return (
+    <div className="resetPasswordModalContainer">
+      <div className="resetPasswordModalBackgroundBlur"></div>
+      <div className="resetPasswordModalContent">
+        <div className="resetPasswordModalHeader">
+          <h2 className="resetPasswordModalTitle">Recuperar Contraseña</h2>
+          <button
+            className="resetPasswordCloseButton"
+            onClick={handleCloseModal}
+          >
+            <FaTimes />
+          </button>
         </div>
-    );
+        <div className="resetPasswordModalBody">
+          {messageBar}
+          <InputGeneral
+            name={"Correo electrónico"}
+            type="text"
+            placeholder=" ej. nombre@gmail.com"
+            width="100%"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="resetPasswordInputField"
+          />
+          <div
+            className="resetPasswordSubmitButton"
+            onClick={handlePasswordReset}
+          >
+            Enviar Correo
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ModalResetPassword;
